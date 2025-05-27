036691123516469a18e6f8a0ad1f1ab523d5be1e
Boris Lončarević <escapetothevoid@hotmail.com>
2025-05-27 02:29:27 +0200

    KvClead
    unused import clean

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { DragDropContext, Droppable, Draggable, DropResult, DragStart, DragUpdate } from '@hello-pangea/dnd';
 import { SongItem } from '@/types/songs';
-import { kv } from '@vercel/kv';
 
 interface SongListProps {
   songItems: SongItem[];
